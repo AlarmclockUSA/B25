ba1187ccb4e82ed9c0b32e5cbf6913473ad1a821
AlarmclockUSA <matt@brilliantperspectives.com>
2025-02-12 15:06:34 -0500

    feat: Update all CTA links to point to virtualticket URL

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,11 +1,10 @@
 export function getCartUrl(searchParams: { [key: string]: string | string[] | undefined }) {
   const el = searchParams.el;
-  const baseUrl = 'https://www.tickets.brilliance25.com';
-  const orderNumber = 'order1737992211982';
+  const baseUrl = 'https://www.tickets.brilliance25.com/virtualticket';
   
   if (el && typeof el === 'string') {
-    return `${baseUrl}/${orderNumber}/?eL=${el}`;
+    return `${baseUrl}?eL=${el}`;
   }
   
-  return `${baseUrl}/${orderNumber}`;
+  return baseUrl;
 } 
